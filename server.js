777d0cc5c6848e872bcdd2d159bed72b381cf662
Antoine Tawil <tony.tawil@nation.dev>
2023-12-18 01:58:49 +0200

    initialized socketio

--- server.js
+++ server.js
@@ -4,7 +4,12 @@ const path = require("path");
 const { MongoClient } = require("mongodb");
 const session = require("express-session");
 
+const http = require("http");
+const socketio = require("socket.io");
+
 const app = express();
+const server = http.createServer(app);
+const io = socketio(server);
 
 app.use(bodyParser.json());
 
@@ -148,6 +153,54 @@ app.put("/api/user", async (req, res) => {
   }
 });
 
-app.listen(3001, () => {
-  console.log("Server is running at port 3000");
+// ... rest of your code ...
+
+let games = {}; // Temporary storage for games. Replace with actual database.
+
+io.on("connection", (socket) => {
+  console.log("New client connected");
+
+  socket.on("create", (data, callback) => {
+    // Generate a unique game code
+    const gameCode = Math.random().toString(36).substring(2, 8).toUpperCase();
+
+    // Store the game data along with the game code and creator's socket ID
+    games[gameCode] = {
+      ...data,
+      code: gameCode,
+      creator: socket.id,
+      player: null,
+      active: false,
+    };
+
+    console.log("Game created:", games[gameCode]);
+
+    // Send the game code back to the client
+    callback({ success: true, code: gameCode });
+  });
+
+  socket.on("join", (gameCode, callback) => {
+    const game = games[gameCode];
+
+    if (game && !game.active) {
+      // If the game exists and is not active, add the player to the game
+      game.player = socket.id;
+      game.active = true;
+
+      // Notify both players that the game has started
+      io.to(game.creator).emit("gameStarted", game);
+      io.to(game.player).emit("gameStarted", game);
+
+      callback({ success: true });
+    } else {
+      // If the game does not exist or is already active, send an error
+      callback({ error: "Invalid game code or game already started" });
+    }
+  });
+
+  // ... rest of your code ...
+});
+
+server.listen(3001, () => {
+  console.log("Server is running at port 3001");
 });
